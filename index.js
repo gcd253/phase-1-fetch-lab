92d7fd405836b7287f68da1c22b9130285680488
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-24 11:41:41 -0400

    commit tree 50af9eb429615afda3faa79c15e357d3f7628c70

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  ;{
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    
+  };
   
 }
 
@@ -13,5 +19,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
